328fe5833ca25176abd85043ef0138223015adf9
naniassociates <naniassociates2017@gmail.com>
2025-08-27 16:22:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,3 +13,18 @@ def load_data():
 
 def save_data(df):
     df.to_csv(FILE_NAME, index=False)
+import pandas as pd
+
+FILE_NAME = "data.csv"
+
+def load_data():
+    try:
+        return pd.read_csv(FILE_NAME)
+    except FileNotFoundError:
+        return pd.DataFrame(columns=[
+            "Date", "Type", "Customer", "Service", "Applications", "Expense", "Income",
+            "Profit", "Payment Status", "Amount Received", "Pending Amount", "Remarks"
+        ])
+
+def save_data(df):
+    df.to_csv(FILE_NAME, index=False)
